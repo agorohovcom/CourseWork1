99355059b7baf93beec9f8d36f8bc42d90efed5d
agorohov <aleksandr.gorohov@gmail.com>
2024-03-27 09:08:01 +0300

    тест 1

---
+++ src/Employee.java
@@ -0,0 +1,67 @@
+import java.util.Objects;
+
+public class Employee {
+    private final String fullName;
+    private int department;
+    private int salary;
+    private final int id;
+
+    private static short employeeCounter = 1;
+
+    public Employee(String fullName, int department, int salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        id = employeeCounter++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getEmployeeCounter() {
+        return employeeCounter;
+    }
+
+    public void setDepartment(short department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник номер " + id
+                + " {ФИО: " + fullName
+                + ", отдел: " + department
+                + ", зарплата: " + salary
+                + '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Курсовая работа 1");
+        Employee[] employees = new Employee[10];
+
+        employees[0] = new Employee("Петров Петр Петрович", 1, 3300);
+        employees[1] = new Employee("Антонов Антон Антонович", 2, 5500);
+        employees[2] = new Employee("Станиславов Станислав Станиславович", 3, 1100);
+        employees[3] = new Employee("Янов Ян Янович", 4, 7700);
+
+        printAllEmployees(employees);
+    }
+
+    public Employee[] employees = new Employee[10];
+
+    public static void printAllEmployees(Employee[] employees) {
+        for (Employee e : employees) {
+            if (e != null) {
+                System.out.println(e);
+            }
+        }
     }
 }
