88ec84286f5311299a8124d3018d615252da2654
agorohov <aleksandr.gorohov@gmail.com>
2024-03-27 11:21:01 +0300

    Базовая сложность complete

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,29 @@ public class Main {
         employees[0] = new Employee("Петров Петр Петрович", 1, 3300);
         employees[1] = new Employee("Антонов Антон Антонович", 2, 5500);
         employees[2] = new Employee("Станиславов Станислав Станиславович", 3, 1100);
-        employees[3] = new Employee("Янов Ян Янович", 4, 7700);
+        employees[3] = new Employee("Янов Ян Янович", 3, 7700);
+        employees[5] = new Employee("Борисов Борис Борисович", 4, 8800);
+        employees[7] = new Employee("Залипупов Залипуп Залипупович", 1, 4400);
 
+        System.out.println("Список сотрудников со всеми данными:");
         printAllEmployees();
+        System.out.println();
+
         System.out.println("Сумма затрат на ЗП в месяц: " + countMonthSalaries());
+        System.out.println();
+
         System.out.println("Сотрудник с минимальной ЗП: " + getEmployeeWithMinSalary());
+        System.out.println();
+
+        System.out.println("Сотрудник с максимальной ЗП: " + getEmployeeWithMaxSalary());
+        System.out.println();
+
+        System.out.printf("Среднее значение ЗП: %.2f\n", averageMonthSalary());
+        System.out.println();
+
+        System.out.println("Список ФИО всех сотрудников:");
+        printAllEmployeesFullNames();
+        System.out.println();
     }
 
     public static Employee[] employees = new Employee[10];
@@ -43,5 +61,32 @@ public class Main {
             throw new RuntimeException("Нет ни одного сотрудника");
         }
         return empWithMinSalary;
+    }
+
+    public static Employee getEmployeeWithMaxSalary() {
+        Employee empWithMaxSalary = null;
+        double maxSalary = Double.MIN_VALUE;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() > maxSalary) {
+                maxSalary = employee.getSalary();
+                empWithMaxSalary = employee;
+            }
+        }
+        if (empWithMaxSalary == null) {
+            throw new RuntimeException("Нет ни одного сотрудника");
+        }
+        return empWithMaxSalary;
+    }
+
+    public static double averageMonthSalary() {
+        return countMonthSalaries() / (Employee.getEmployeeCounter() - 1);
+    }
+
+    public static void printAllEmployeesFullNames() {
+        for (Employee e : employees) {
+            if (e != null) {
+                System.out.println(e.getFullName());
+            }
+        }
     }
 }
